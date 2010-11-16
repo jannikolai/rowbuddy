d8f58e5345d8c3d0032563f73ee526a9b0167cda
Fleischer.Georg <Fleischer.Georg@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-11-16 14:02:37 +0000

    BoatManagement

--- 20_sources/EJB Project/ejbModule/de/rowbuddy/business/BoatManagement.java
+++ 20_sources/EJB Project/ejbModule/de/rowbuddy/business/BoatManagement.java
@@ -1,6 +1,10 @@
 package de.rowbuddy.business;
 
+import java.util.Collection;
+
 import javax.ejb.Stateless;
+
+import de.rowbuddy.entities.Boat;
 
 @Stateless
 public class BoatManagement {
@@ -8,5 +12,20 @@ public class BoatManagement {
 	public BoatManagement() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public Collection<BoatOverview> getBoatOverview(){
+		throw new InYourFaceException();
+	}
+	
+	public Boat getBoat(long id){
+		throw new InYourFaceException();
+	}
+	
+	public Boat addBoat(Boat boat){
+		throw new InYourFaceException();
+	}
+	
+	public void removeBoat(long id){
+		throw new InYourFaceException();
+	}
 }
---
+++ 20_sources/EJB Project/ejbModule/de/rowbuddy/business/InYourFaceException.java
@@ -0,0 +1,9 @@
+package de.rowbuddy.business;
+
+public class InYourFaceException extends RuntimeException {
+
+	public InYourFaceException() {
+		super("In your Face!");
+	}
+	
+}
