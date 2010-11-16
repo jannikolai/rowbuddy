de988ebc061443e9551e78f6451f74624d7428eb
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-11-16 17:49:21 +0000

    events added

---
+++ 20_sources/Web Project/src/de/rowbuddy/client/events/AddBoatEventHandler.java
@@ -0,0 +1,8 @@
+package de.rowbuddy.client.events;
+
+import com.google.gwt.event.shared.EventHandler;
+
+
+public interface AddBoatEventHandler extends EventHandler{
+	void onAddBoatEvent(AddBoatEvent event);
+}
