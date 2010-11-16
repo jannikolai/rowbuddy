e10c78d1424bc5141da1b4f3e776f25b12f72e90
lydia.wall@web.de <lydia.wall@web.de@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-11-16 20:17:47 +0000

    change pageDesign

--- trunk/20_sources/Web Project/src/de/rowbuddy/client/GWTEntryPoint.java
+++ trunk/20_sources/Web Project/src/de/rowbuddy/client/GWTEntryPoint.java
@@ -40,11 +40,14 @@ public class GWTEntryPoint implements EntryPoint {
 		flexTable.setCellPadding(3);
 		
 		cellFormatter.setHorizontalAlignment(0, 0, HasHorizontalAlignment.ALIGN_LEFT);
-		cellFormatter.setHorizontalAlignment(0, 2, HasHorizontalAlignment.ALIGN_RIGHT);
 		cellFormatter.setColSpan(0, 0, 2);
+		cellFormatter.setHorizontalAlignment(0, 2, HasHorizontalAlignment.ALIGN_RIGHT);
 		cellFormatter.setVerticalAlignment(1, 0, HasVerticalAlignment.ALIGN_TOP);
+		cellFormatter.setRowSpan(1, 0, 2);
 		cellFormatter.setHorizontalAlignment(1, 1, HasHorizontalAlignment.ALIGN_LEFT);
 		cellFormatter.setColSpan(1, 1, 2);
+		cellFormatter.setHorizontalAlignment(2, 1, HasHorizontalAlignment.ALIGN_LEFT);
+		cellFormatter.setColSpan(2, 1, 2);
 		
 		
 		Label headerLabel = new Label("HEADER");
@@ -56,11 +59,15 @@ public class GWTEntryPoint implements EntryPoint {
 		flexTable.setWidget(0, 2, loginLabel);
 		
 		StackPanel menuPanel = new StackPanel();
-		menuPanel.setHeight("30em");
+		menuPanel.setHeight("20em");
 		flexTable.setWidget(1, 0, menuPanel);
 		
 		Label messagesLabel = new Label("messages");
+		messagesLabel.setHeight("10em");
 		flexTable.setWidget(1, 1, messagesLabel);	
+		
+		Label mainLabel = new Label("main");
+		flexTable.setWidget(2, 1, mainLabel);
 
 		return flexTable;
 	}
