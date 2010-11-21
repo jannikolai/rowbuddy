2f2a1ae3fcd47d6c83901c21b6896e2782400ee7
Fleischer.Georg <Fleischer.Georg@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-11-21 20:11:40 +0000

    added logic to impl.

--- 20_sources/Web Project/src/de/rowbuddy/server/web/BoatRemoteServiceImpl.java
+++ 20_sources/Web Project/src/de/rowbuddy/server/web/BoatRemoteServiceImpl.java
@@ -4,14 +4,20 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.ejb.EJB;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
+import de.rowbuddy.business.BoatManagement;
 import de.rowbuddy.business.dtos.BoatDTO;
 import de.rowbuddy.client.services.BoatRemoteService;
 import de.rowbuddy.entities.Boat;
 
 public class BoatRemoteServiceImpl extends RemoteServiceServlet implements BoatRemoteService{
 
+	@EJB
+	BoatManagement boatManagement;
+	
 	/**
 	 * 
 	 */
@@ -19,27 +25,11 @@ public class BoatRemoteServiceImpl extends RemoteServiceServlet implements BoatR
 
 	@Override
 	public Collection<BoatDTO> getBoatOverview() {
-		// TODO Auto-generated method stub
-		List<BoatDTO> boats = new LinkedList<BoatDTO>();
-		BoatDTO boat1 = new BoatDTO();
-		boat1.setId(1);
-		boat1.setLocked(false);
-		boat1.setCoxed(true);
-		boat1.setName("Unsinkbar 3");
-		boat1.setNumberOfSeats(5);
-		boats.add(boat1);
-		boats.add(boat1);
-		boats.add(boat1);
-		
-		return boats;
+		return boatManagement.getBoatOverview();
 	}
 
 	@Override
 	public void addBoat(Boat boat) {
-		// TODO Auto-generated method stub
-		
+		boatManagement.addBoat(boat);
 	}
-	
-	
-
 }
