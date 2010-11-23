a7d483c5664c79a603c22f617fb8de4bbb675ed5
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-11-23 18:44:26 +0000

    serlizable added

--- 20_sources/EJB Project/ejbModule/de/rowbuddy/exceptions/RowBuddyException.java
+++ 20_sources/EJB Project/ejbModule/de/rowbuddy/exceptions/RowBuddyException.java
@@ -1,10 +1,21 @@
 package de.rowbuddy.exceptions;
 
+import java.io.Serializable;
+
 import javax.ejb.ApplicationException;
 
 @ApplicationException
-public class RowBuddyException extends Exception {
+public class RowBuddyException extends Exception implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	public RowBuddyException() {
+	}
+	
 	public RowBuddyException(String message) {
 		super(message);
 	}
