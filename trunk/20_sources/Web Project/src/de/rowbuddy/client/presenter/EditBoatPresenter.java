4d3d06ffa618b8d818292f1c27e74b0725b09b61
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-11-23 18:42:50 +0000

    exception added

--- trunk/20_sources/Web Project/src/de/rowbuddy/client/presenter/EditBoatPresenter.java
+++ trunk/20_sources/Web Project/src/de/rowbuddy/client/presenter/EditBoatPresenter.java
@@ -6,6 +6,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.HasWidgets;
@@ -16,30 +17,38 @@ import de.rowbuddy.client.services.BoatRemoteServiceAsync;
 import de.rowbuddy.entities.Boat;
 import de.rowbuddy.exceptions.RowBuddyException;
 
-public class EditBoatPresenter implements Presenter{
-	public interface Display{
+public class EditBoatPresenter implements Presenter {
+	public interface Display {
 		HasClickHandlers getSubmitButton();
+
 		HasClickHandlers getCancelButton();
+
 		HasValue<String> getName();
+
 		HasValue<String> getNumberOfSeats();
+
 		HasValue<Boolean> isCoxed();
+
+		HasValue<Boolean> isLocked();
+
 		Widget asWidget();
 	}
-	
+
 	private Display view;
 	private BoatRemoteServiceAsync boatService;
 	private SimpleEventBus eventBus;
 	private Boat boat;
 	private Long id;
 	private Logger logger = Logger.getLogger(EditBoatPresenter.class.getName());
-	
-	public EditBoatPresenter(Display view, BoatRemoteServiceAsync boatService, SimpleEventBus eventBus, Long id){
+
+	public EditBoatPresenter(Display view, BoatRemoteServiceAsync boatService,
+			SimpleEventBus eventBus, Long id) {
 		this.view = view;
 		this.boatService = boatService;
 		this.eventBus = eventBus;
 		this.id = id;
 	}
-		
+
 	@Override
 	public void start(HasWidgets container) {
 		// TODO Auto-generated method stub
@@ -47,63 +56,75 @@ public class EditBoatPresenter implements Presenter{
 		bind();
 		fetchBoat();
 		container.add(view.asWidget());
-		
+
 	}
-	
-	private void fetchBoat(){
+
+	private void fetchBoat() {
 		logger.info("Get Boat id:" + id);
 		boatService.getBoat(id, new AsyncCallback<Boat>() {
-			
+
 			@Override
 			public void onSuccess(Boat arg0) {
 				boat = arg0;
 				logger.info("Boat feteched! id:" + boat.getId());
 				view.getName().setValue(boat.getName());
-				view.getNumberOfSeats().setValue(String.valueOf(boat.getNumberOfSeats()));
+				view.getNumberOfSeats().setValue(
+						String.valueOf(boat.getNumberOfSeats()));
 				view.isCoxed().setValue(boat.isCoxed());
+				view.isLocked().setValue(boat.isLocked());
 			}
-			
+
 			@Override
 			public void onFailure(Throwable arg0) {
 				logger.severe("Cannot fetch boat:" + arg0.getMessage());
 			}
 		});
 	}
-	
-	private void bind(){
+
+	private void updateBoat() {
+		try {
+			boat.setName(view.getName().getValue());
+			boat.setCoxed(view.isCoxed().getValue());
+			int seats = Integer.valueOf(view.getNumberOfSeats().getValue());
+
+			boat.setNumberOfSeats(seats);
+			boat.setLocked(view.isLocked().getValue());
+			logger.info("Update boat with id:" + boat.getId());
+		} catch (NumberFormatException ex) {
+			logger.warning("Seat is not a number");
+		} catch (RowBuddyException ex) {
+			logger.warning(ex.getMessage());
+		}
+	}
+
+	private void bind() {
 		view.getSubmitButton().addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent arg0) {
-				try{
-					try {
-						boat.setName(view.getName().getValue());
-					} catch (RowBuddyException e) {
-						e.printStackTrace();
+				updateBoat();
+				boatService.updateBoat(boat, new AsyncCallback<Void>() {
+
+					@Override
+					public void onSuccess(Void arg0) {
+						logger.info("Submit successful GoTo ListBoats");
+						eventBus.fireEvent(new ListBoatEvent());
 					}
-					boat.setCoxed(view.isCoxed().getValue());
-					int seats = Integer.valueOf(view.getNumberOfSeats().getValue());
-					try {
-						boat.setNumberOfSeats(seats);
-					} catch (RowBuddyException e) {
-						e.printStackTrace();
+
+					@Override
+					public void onFailure(Throwable arg0) {
+						logger.warning("Cannout update Boat:"
+								+ arg0.getMessage());
 					}
-					logger.info("Update boat with id:" + boat.getId());
-					boatService.updateBoat(boat, new AsyncCallback<Void>() {
-						@Override
-						public void onSuccess(Void arg0) {
-							logger.info("Submit successful GOTO List");
-							eventBus.fireEvent(new ListBoatEvent());
-						}
-						
-						@Override
-						public void onFailure(Throwable arg0) {
-							logger.warning("Cannout update Boat:" + arg0.getMessage());
-						}
-					});
-					
-				} catch(NumberFormatException ex){
-					logger.warning("Seat is not a number");
-				}
+				});
+
+			}
+		});
+
+		view.getCancelButton().addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent arg0) {
+				History.back();
 			}
 		});
 	}
