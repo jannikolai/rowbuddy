8b9c6adb057c951841634b3ba178b4be5dcfc531
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-11-27 09:31:59 +0000

    menu actions defined

--- 20_sources/Web Project/src/de/rowbuddy/client/presenter/MenuPresenter.java
+++ 20_sources/Web Project/src/de/rowbuddy/client/presenter/MenuPresenter.java
@@ -3,38 +3,52 @@ package de.rowbuddy.client.presenter;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 
 import de.rowbuddy.client.HistoryConstants;
+import de.rowbuddy.client.events.AddBoatEvent;
+import de.rowbuddy.client.events.ListBoatEvent;
 
 public class MenuPresenter implements Presenter, HistoryConstants{
 	
 	public interface MenuDisplay {
 		public HasClickHandlers getListBoatButton();
+		public HasClickHandlers getAddBoat();
 		public Widget asWidget();
 	}
 	private MenuDisplay view;
 	private HasWidgets container;
+	private SimpleEventBus eventBus;
 	
-	public MenuPresenter(MenuDisplay view) {
+	public MenuPresenter(MenuDisplay view, SimpleEventBus eventBus) {
 		this.view = view;
+		this.eventBus = eventBus;
 	}
 	
 	@Override
 	public void start(HasWidgets container) {
+		bind();
 		this.container = container;
-		container.clear();
-		container.add(view.asWidget());
+		this.container.clear();
+		this.container.add(view.asWidget());
 	}
 	
 	private void bind(){
 		view.getListBoatButton().addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent arg0) {
+				eventBus.fireEvent(new ListBoatEvent());
+			}
+		});
+		
+		view.getAddBoat().addClickHandler(new ClickHandler() {
 			
 			@Override
 			public void onClick(ClickEvent arg0) {
-				History.newItem(LIST_BOATS);
+				eventBus.fireEvent(new AddBoatEvent());
 			}
 		});
 		
