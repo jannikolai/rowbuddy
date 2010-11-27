a827bc987777aef91c06ed6e96e25c9fa7460553
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-11-27 11:46:28 +0000

    fade in/out effekt

---
+++ 20_sources/Web Project/src/de/rowbuddy/client/FadeAnimation.java
@@ -0,0 +1,39 @@
+package de.rowbuddy.client;
+
+import com.google.gwt.animation.client.Animation;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.Widget;
+
+import de.rowbuddy.client.presenter.Presenter;
+
+public class FadeAnimation extends Animation {
+
+	private Widget widget;
+	private Presenter presenter;
+	private boolean started = false;
+	
+
+	public FadeAnimation(HasWidgets widget, Presenter presenter) {
+		if (widget instanceof Widget) {
+			this.widget = (Widget) widget;
+		} else {
+			throw new IllegalArgumentException("Widget required");
+		}
+		this.presenter = presenter;
+	}
+
+	@Override
+	protected void onUpdate(double arg0) {
+		if (arg0 < 0.5) {
+			widget.getElement().getStyle().setOpacity(1 - (arg0 * 2));
+		} else {
+			if(!started) {
+			presenter.start((HasWidgets) widget);
+			started = true;
+			}
+			widget.getElement().getStyle().setOpacity((arg0 - 0.5) * 2);
+		}
+
+	}
+
+}
