ee7813094e11ebfa728647382e77e5adaa69f8a6
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-05 10:08:37 +0000

    exception removed

--- trunk/20_sources/Web Project/src/de/rowbuddy/client/views/boat/DamageView.java
+++ trunk/20_sources/Web Project/src/de/rowbuddy/client/views/boat/DamageView.java
@@ -1,32 +1,86 @@
 package de.rowbuddy.client.views.boat;
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
+import com.google.gwt.user.client.ui.CheckBox;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DecoratorPanel;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTMLTable;
 import com.google.gwt.user.client.ui.HasValue;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
+import de.rowbuddy.client.PageTitles;
 import de.rowbuddy.client.presenter.boat.ListDamagePresenter.Display;
 
-public class DamageView implements Display{
+public class DamageView extends Composite implements Display{
 
-	public DamageView(){
-		
-	}
+	private DecoratorPanel decorator;
+	private FlexTable contentTable;
+	private CheckBox checkBox;
+	private FlexTable damageTable;
+	int index = 1;
 	
-	@Override
-	public HasValue<Boolean> getHistory() {
-		// TODO Auto-generated method stub
-		return null;
+	public DamageView(){
+		decorator = new DecoratorPanel();
+		contentTable = new FlexTable();
+		contentTable.setStylePrimaryName("contentTable");
+
+		initWidget(decorator);
+		decorator.add(contentTable);
+
+		HorizontalPanel hPanel = new HorizontalPanel();
+		hPanel.setHorizontalAlignment(HorizontalPanel.ALIGN_LEFT);
+		checkBox = new CheckBox("Alle Schäden");
+		hPanel.add(checkBox);
+		contentTable.setWidget(2, 0, hPanel);
+
+		damageTable = new FlexTable();
+		damageTable.setWidth("100%");
+		damageTable.setStyleName("boatTable");
+
+		contentTable.setWidget(1, 0, damageTable);
+		contentTable.setText(0, 0, PageTitles.BOAT_DAMAGES);
+		HTMLTable.RowFormatter rf = contentTable.getRowFormatter();
+		rf.setStylePrimaryName(0, "pageHeadLine");
 	}
 
 	@Override
 	public void addDamageRow(String name, String reporter, String date,
 			boolean isOpen) {
-		// TODO Auto-generated method stub
-		
+		damageTable.setText(index, 0, name);
+		damageTable.setText(index, 1, reporter);
+		damageTable.setText(index, 2, date);
+		CheckBox box = new CheckBox();
+		box.setEnabled(isOpen);
+		damageTable.setWidget(index, 3, box);
+		index++;
 	}
-
+	
+	
 	@Override
 	public Widget asWidget() {
-		// TODO Auto-generated method stub
-		return null;
+		return this;
+	}
+
+
+	@Override
+	public void clear() {
+		index = 1;
+		initTableHeader();
+	}
+	
+	private void initTableHeader(){
+		damageTable.setText(0, 0, "Name");
+		damageTable.setText(0, 1, "Gemeldet von");
+		damageTable.setText(0, 2, "Erstellt am");
+		damageTable.setText(0, 3, "Offen");
+		damageTable.getRowFormatter().setStyleName(0, "boatTableHeader");
+	}
+
+
+	@Override
+	public HasValueChangeHandlers<Boolean> historyEnabled() {
+		return checkBox;
 	}
 
 }
