ed8b0f35a197aa24fb403c7c7f208afce06dd928
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-05 10:08:37 +0000

    exception removed

--- 20_sources/Web Project/src/de/rowbuddy/client/PageTitles.java
+++ 20_sources/Web Project/src/de/rowbuddy/client/PageTitles.java
@@ -5,6 +5,7 @@ public interface PageTitles {
 	public final String BOAT_EDIT = "Boot bearbeiten";
 	public final String BOAT_ADD = "Boot hinzufügen";
 	public final String BOAT_DETAIL = "Boot details";
+	public final String BOAT_DAMAGES = "Bootschäden";
 	public final String LOGBOOK_PERSONAL_TRIPS = "Eigene Fahrten";
 	public final String LOGBOOK_PERSONAL_OPEN_TRIPS = "Eigene offene Fahrten";
 }
--- 20_sources/Web Project/src/de/rowbuddy/client/presenter/boat/ListDamagePresenter.java
+++ 20_sources/Web Project/src/de/rowbuddy/client/presenter/boat/ListDamagePresenter.java
@@ -1,5 +1,8 @@
 package de.rowbuddy.client.presenter.boat;
 
+import com.google.gwt.event.logical.shared.HasValueChangeHandlers;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.SimpleEventBus;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.HasWidgets;
@@ -11,8 +14,9 @@ import de.rowbuddy.client.services.BoatRemoteServiceAsync;
 public class ListDamagePresenter implements Presenter{
 	
 	public interface Display{
-		HasValue<Boolean> getHistory();
+		HasValueChangeHandlers<Boolean> historyEnabled();
 		void addDamageRow(String name, String reporter, String date, boolean isOpen);
+		void clear();
 		Widget asWidget();
 	}
 	
@@ -30,6 +34,17 @@ public class ListDamagePresenter implements Presenter{
 	public void start(HasWidgets container) {
 		container.clear();
 		container.add(view.asWidget());
+		bind();
 	}
-
+	
+	private void bind(){
+		view.historyEnabled().addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+			
+			@Override
+			public void onValueChange(ValueChangeEvent<Boolean> arg0) {
+				
+			}
+		});
+	}
+	
 }
