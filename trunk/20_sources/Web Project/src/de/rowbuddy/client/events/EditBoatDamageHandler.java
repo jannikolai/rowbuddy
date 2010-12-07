e5ccff72b3cebc19c900d497b20049be0c8134da
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-07 10:28:49 +0000

    damage addedd

---
+++ trunk/20_sources/Web Project/src/de/rowbuddy/client/events/EditBoatDamageHandler.java
@@ -0,0 +1,7 @@
+package de.rowbuddy.client.events;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface EditBoatDamageHandler extends EventHandler{
+	public void onEditBoatDamage();
+}
