7dbfcdcf27f87c5cdd5505a41d89f42d99a0e507
lydia.wall@web.de <lydia.wall@web.de@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-07 09:38:53 +0000

    add css Dialogbox

--- trunk/20_sources/Web Project/src/de/rowbuddy/client/views/boat/EditBoatView.java
+++ trunk/20_sources/Web Project/src/de/rowbuddy/client/views/boat/EditBoatView.java
@@ -5,11 +5,8 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.DecoratorPanel;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HTMLTable;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.TextBox;
@@ -18,76 +15,80 @@ import de.rowbuddy.client.PageTitles;
 import de.rowbuddy.client.presenter.boat.EditBoatPresenter;
 import de.rowbuddy.client.views.HeaderButtonView;
 
-public class EditBoatView extends HeaderButtonView implements EditBoatPresenter.Display{
+public class EditBoatView extends HeaderButtonView implements
+		EditBoatPresenter.Display {
 
 	private Button submitButton;
 	private Button cancelButton;
 	private FlexTable boatTable;
 	private TextBox nameText;
 	private TextBox numberOfSeats;
-	private CheckBox coxed; 
+	private CheckBox coxed;
 	private CheckBox locked;
 	private DialogBox box;
 	private Button deleteButton;
 	private Button dialogButton;
-	
-	public EditBoatView(){
+
+	public EditBoatView() {
 		super(PageTitles.BOAT_EDIT);
-		
-		submitButton= new Button("Boot speichern");
+
+		submitButton = new Button("Boot speichern");
 		submitButton.setStylePrimaryName("buttonSave buttonPositive");
 		cancelButton = new Button("Abbrechen");
 		cancelButton.setStylePrimaryName("buttonCancel buttonNegative");
 		dialogButton = new Button("Löschen");
 		dialogButton.setStylePrimaryName("buttonDelete buttonNegative");
-		
+
 		addButton(submitButton);
 		addButton(dialogButton);
 		addButton(cancelButton);
-	
+
 		boatTable = new FlexTable();
-		
+
 		boatTable.setText(0, 0, "Name:");
 		nameText = new TextBox();
 		boatTable.setWidget(0, 1, nameText);
-		
+
 		boatTable.setText(1, 0, "Bootsplätze (ohne Steuermann):");
 		numberOfSeats = new TextBox();
 		boatTable.setWidget(1, 1, numberOfSeats);
-		
+
 		boatTable.setText(2, 0, "Gesteuert:");
 		coxed = new CheckBox();
 		boatTable.setWidget(2, 1, coxed);
-		
+
 		boatTable.setText(3, 0, "Gesperrt:");
 		locked = new CheckBox();
 		boatTable.setWidget(3, 1, locked);
-		
-		box = new DialogBox(false, true);
-	    box.setGlassEnabled(true);
-	    box.setAnimationEnabled(true);
-	    deleteButton = new Button("Ja");
 
-	    HorizontalPanel dialogContent = new HorizontalPanel();
-	    
-	    box.setText("Möchten Sie wirklich löschen?");
-	    box.setWidget(dialogContent);
-	    dialogContent.add(deleteButton);
-	    Button dialogClose = new Button("Nein");
-	    dialogClose.addClickHandler(new ClickHandler() {
-			
+		box = new DialogBox(false, true);
+		box.setGlassEnabled(true);
+		box.setGlassStyleName("glassStyle");
+		box.setAnimationEnabled(true);
+		deleteButton = new Button("Ja");
+		deleteButton.setStylePrimaryName("buttonApply buttonPositive");
+
+		HorizontalPanel dialogContent = new HorizontalPanel();
+
+		box.setText("Möchten Sie wirklich löschen?");
+		box.setWidget(dialogContent);
+		dialogContent.add(deleteButton);
+		Button dialogClose = new Button("Nein");
+		dialogClose.setStylePrimaryName("buttonCancel buttonNegative");
+		dialogClose.addClickHandler(new ClickHandler() {
+
 			@Override
 			public void onClick(ClickEvent arg0) {
 				// TODO Auto-generated method stub
 				closeDialog();
 			}
 		});
-	    
-	    dialogContent.add(dialogClose);
-	    
+
+		dialogContent.add(dialogClose);
+
 		setContent(boatTable);
 	}
-	
+
 	@Override
 	public HasClickHandlers getSubmitButton() {
 		return submitButton;
@@ -117,22 +118,22 @@ public class EditBoatView extends HeaderButtonView implements EditBoatPresenter.
 	public HasValue<Boolean> isLocked() {
 		return locked;
 	}
-	
+
 	@Override
-	public HasClickHandlers getPopUpButton(){
+	public HasClickHandlers getPopUpButton() {
 		return dialogButton;
 	}
-	
+
 	@Override
-	public void showPopUp(){
+	public void showPopUp() {
 		box.center();
 	}
-	
+
 	@Override
 	public HasClickHandlers getDeleteButton() {
 		return deleteButton;
 	}
-	
+
 	@Override
 	public void closeDialog() {
 		box.hide();
