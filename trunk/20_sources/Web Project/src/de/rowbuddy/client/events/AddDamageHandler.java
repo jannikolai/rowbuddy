5152fac896bbb6d9b21dd6794e2fd47058ac55a3
Fleischer.Georg <Fleischer.Georg@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-08 21:47:46 +0000

    New EventHandling: AddDamage, DetailDamage

--- trunk/20_sources/Web Project/src/de/rowbuddy/client/events/AddDamageHandler.java
+++ trunk/20_sources/Web Project/src/de/rowbuddy/client/events/AddDamageHandler.java
@@ -1,7 +1,43 @@
 package de.rowbuddy.client.events;
 
-import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.gwt.user.client.ui.HasWidgets;
 
-public interface AddDamageHandler extends EventHandler {
-	void onAddDamage(AddDamageEvent event);
+import de.rowbuddy.client.presenter.Presenter;
+import de.rowbuddy.client.presenter.boat.AddDamagePresenter;
+import de.rowbuddy.client.services.BoatRemoteServiceAsync;
+import de.rowbuddy.client.views.boat.AddDamageView;
+
+public class AddDamageHandler extends AbstractEventHandler {
+
+	private final BoatRemoteServiceAsync boatService;
+
+	public AddDamageHandler(HasWidgets targetWidget, EventBus eventBus,
+			BoatRemoteServiceAsync boatService) {
+		super(targetWidget, eventBus);
+
+		this.boatService = boatService;
+	}
+
+	@Override
+	public AbstractEvent<?> toEvent(String historyItem) {
+		return new AddDamageEvent();
+	}
+
+	@Override
+	public Presenter createPresenter(AbstractEvent<?> event) {
+		return new AddDamagePresenter(boatService, eventBus,
+				new AddDamageView());
+	}
+
+	@Override
+	protected <T extends AbstractEventHandler> Type<T> getType() {
+		return (Type<T>) AddDamageEvent.TYPE;
+	}
+
+	@Override
+	protected String getHistoryIdentifier() {
+		return AddDamageEvent.HISTORY_IDENTIFIER;
+	}
 }
