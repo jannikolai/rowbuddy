2cb7c0550c00d25ad7262c68d4e38de17555fd17
Fleischer.Georg <Fleischer.Georg@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-10 23:10:48 +0000

    Added RouteManagement Business Logic

--- trunk/20_sources/EJB Project/ejbModule/de/rowbuddy/entities/Route.java
+++ trunk/20_sources/EJB Project/ejbModule/de/rowbuddy/entities/Route.java
@@ -1,103 +1,150 @@
 package de.rowbuddy.entities;
 
 import java.io.Serializable;
-import java.lang.String;
+import java.util.LinkedList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import de.rowbuddy.exceptions.RowBuddyException;
 
 /**
  * Entity implementation class for Entity: Route
- *
+ * 
  */
 @Entity
 public class Route implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id = null;
+	private Long parentId = null;
 	private String name = "";
 	private String shortDescription = "";
-	private long versionNumber = 1;
 	@OneToMany
-	private List<GpsPoint> wayPoints;
+	private List<GpsPoint> wayPoints = new LinkedList<GpsPoint>();
 	private double lengthKM = 0;
-	private boolean mutable = false;
+	private Member lastEditor = null;
+	private boolean mutable = true;
+	private boolean deleted = false;
 	private static final long serialVersionUID = 1L;
 
 	public Route() {
 		super();
-	}   
+	}
+
 	public Long getId() {
 		return this.id;
 	}
 
 	public void setId(Long id) {
+		if (id == null) {
+			throw new NullPointerException("Id cannot be null");
+		}
 		this.id = id;
-	}   
+	}
+
+	public Long getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Long parentId) {
+		if (parentId == null) {
+			throw new NullPointerException("Parent id cannot be null");
+		}
+		this.parentId = parentId;
+	}
+
 	public String getName() {
 		return this.name;
 	}
 
 	public void setName(String name) throws RowBuddyException {
-		if (name == null){
+		if (name == null) {
 			throw new NullPointerException("Name cannot be null");
 		}
-		if(name.isEmpty()) {
+		if (name.isEmpty()) {
 			throw new RowBuddyException("Name has to be set");
 		}
 		this.name = name;
 	}
-	
+
 	public String getShortDescription() {
 		return this.shortDescription;
 	}
 
-	public void setShortDescription(String shortDescription) throws RowBuddyException {
-		if (shortDescription == null){
+	public void setShortDescription(String shortDescription)
+			throws RowBuddyException {
+		if (shortDescription == null) {
 			throw new NullPointerException("Short description cannot be null");
 		}
-		if(shortDescription.isEmpty()) {
+		if (shortDescription.isEmpty()) {
 			throw new RowBuddyException("Short Description has to be set");
 		}
 		this.shortDescription = shortDescription;
-	}  
-	
-	public Long getVersionNumber() {
-		return this.versionNumber;
 	}
-	
-	public void setVersionNumber(Long versionNumber) {
-		this.versionNumber = versionNumber;
-	}   
-	
+
+	public Member getLastEditor() {
+		return lastEditor;
+	}
+
+	public void setLastEditor(Member lastEditor) {
+		this.lastEditor = lastEditor;
+	}
+
 	public void setWayPoints(List<GpsPoint> wayPoints) {
+		if (wayPoints == null) {
+			throw new NullPointerException("Waypoints cannot be null");
+		}
+
 		this.wayPoints = wayPoints;
 	}
-	
+
 	public List<GpsPoint> getWayPoints() {
 		return wayPoints;
 	}
-	
+
 	public double getLengthKM() {
 		return this.lengthKM;
 	}
-	
+
 	public void setLengthKM(double lengthKM) throws RowBuddyException {
-		if(lengthKM < 0) {
-			throw new RowBuddyException("length of route has to bigger than 0 km");
+		if (lengthKM < 0) {
+			throw new RowBuddyException(
+					"length of route has to be at least 0 km");
 		}
 		this.lengthKM = lengthKM;
-	}   
-	
-	public boolean getMutable() {
-		return this.mutable;
 	}
+
+	public boolean isMutable() {
+		return mutable;
+	}
+
 	public void setMutable(boolean mutable) {
 		this.mutable = mutable;
 	}
+
+	public boolean isDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+
+	public void validate() throws RowBuddyException {
+		if (name.trim().isEmpty()) {
+			throw new RowBuddyException("Name must not be empty");
+		}
+		if (lengthKM < 0) {
+			throw new RowBuddyException("Lenght must be at least 0 km");
+		}
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -105,6 +152,7 @@ public class Route implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -121,7 +169,5 @@ public class Route implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-   
+
 }
