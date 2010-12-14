fe6735b123ac38726114b406af6a9c53b8b8f9ab
sebastian.heinecke <sebastian.heinecke@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-14 15:23:54 +0000

    Resolved an issue with the not logged in exception not correctly being serialized

--- trunk/20_sources/EJB Project/ejbModule/de/rowbuddy/exceptions/NotLoggedInException.java
+++ trunk/20_sources/EJB Project/ejbModule/de/rowbuddy/exceptions/NotLoggedInException.java
@@ -1,8 +1,12 @@
 package de.rowbuddy.exceptions;
 
-public class NotLoggedInException extends RowBuddyException {
+import java.io.Serializable;
+
+public class NotLoggedInException extends RowBuddyException implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	public NotLoggedInException(){}
 
 	public NotLoggedInException(String message) {
 		super(message);
