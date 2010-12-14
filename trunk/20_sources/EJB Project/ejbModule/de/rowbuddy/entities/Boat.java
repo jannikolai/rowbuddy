7d8dacb94abcbd5a284639735bce966985114ae9
da.chiaradia <da.chiaradia@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-14 12:48:36 +0000

    exception text translated to german

--- trunk/20_sources/EJB Project/ejbModule/de/rowbuddy/entities/Boat.java
+++ trunk/20_sources/EJB Project/ejbModule/de/rowbuddy/entities/Boat.java
@@ -1,23 +1,26 @@
 package de.rowbuddy.entities;
 
 import java.io.Serializable;
-import java.lang.String;
 import java.util.LinkedList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import de.rowbuddy.exceptions.RowBuddyException;
 
 /**
  * Entity implementation class for Entity: Boat
- *
+ * 
  */
 @Entity
 public class Boat implements Serializable {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id = null;
 	private String name = "";
 	private int numberOfSeats = 0;
@@ -32,7 +35,8 @@ public class Boat implements Serializable {
 
 	public Boat() {
 		super();
-	}   
+	}
+
 	public Long getId() {
 		return this.id;
 	}
@@ -40,32 +44,33 @@ public class Boat implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
 
 	public void setName(String name) throws RowBuddyException {
-		if (name == null){
-			throw new NullPointerException("Name cannot be null");
+		if (name == null) {
+			throw new NullPointerException("Name darf nicht null sein");
 		}
-		if(name.isEmpty()) {
-			throw new RowBuddyException("Name has to be set");
+		if (name.isEmpty()) {
+			throw new RowBuddyException("Name darf nicht leer sind");
 		}
 		this.name = name;
-	} 
-	
+	}
+
 	public int getNumberOfSeats() {
 		return this.numberOfSeats;
 	}
 
 	public void setNumberOfSeats(int numberOfSeats) throws RowBuddyException {
-		if(numberOfSeats <= 0) {
-			throw new RowBuddyException("There has to be at least 1 seat");
+		if (numberOfSeats <= 0) {
+			throw new RowBuddyException(
+					"Ein Boot muss mindestens einen Platz haben");
 		}
 		this.numberOfSeats = numberOfSeats;
-	}   
-	
+	}
+
 	public boolean isCoxed() {
 		return this.coxed;
 	}
@@ -73,66 +78,69 @@ public class Boat implements Serializable {
 	public void setCoxed(boolean coxed) {
 		this.coxed = coxed;
 	}
-	
+
 	public List<BoatDamage> getBoatDamages() {
 		return boatDamages;
 	}
-	
-	public void setBoatDamages(List<BoatDamage> boatDamages)  {
-		if (boatDamages == null){
-			throw new NullPointerException("BoatDamages cannot be null");
+
+	public void setBoatDamages(List<BoatDamage> boatDamages) {
+		if (boatDamages == null) {
+			throw new NullPointerException("Bootschaden darf nicht null sein");
 		}
 		this.boatDamages = boatDamages;
 	}
-	
-	public void addBoatDamage(BoatDamage boatDamage) throws RowBuddyException{
-		if (boatDamage == null){
-			throw new NullPointerException("Damage must contain a value");
+
+	public void addBoatDamage(BoatDamage boatDamage) throws RowBuddyException {
+		if (boatDamage == null) {
+			throw new NullPointerException("Bootsschaden darf nicht null sein");
 		}
-		if (boatDamages.contains(boatDamage)){
-			throw new RowBuddyException("Damage is already belonging to this boat");
+		if (boatDamages.contains(boatDamage)) {
+			throw new RowBuddyException("Der Schaden ist bereits registriert");
 		}
 		boatDamages.add(boatDamage);
 		boatDamage.setBoat(this);
 	}
-	
-	public void removeBoatDamage(BoatDamage boatDamage) throws RowBuddyException{
-		if (boatDamage == null){
-			throw new NullPointerException("Damage must contain a value");
+
+	public void removeBoatDamage(BoatDamage boatDamage)
+			throws RowBuddyException {
+		if (boatDamage == null) {
+			throw new NullPointerException("Bootsschaden darf nicht null sein");
 		}
-		if (!boatDamages.contains(boatDamage)){
-			throw new RowBuddyException("Damage does not belong to this boat");
+		if (!boatDamages.contains(boatDamage)) {
+			throw new RowBuddyException(
+					"Bootsschaden gehoert nicht zu diesem Boot");
 		}
 		boatDamages.remove(boatDamage);
 		boatDamage.removeBoat();
 	}
-	
+
 	public List<BoatReservation> getBoatReservations() {
 		return boatReservations;
 	}
-	
-	public void addBoatReservation(BoatReservation boatReservation){
-		if (boatReservation == null){
-			throw new NullPointerException("BoatReservation cannot be null");
+
+	public void addBoatReservation(BoatReservation boatReservation) {
+		if (boatReservation == null) {
+			throw new NullPointerException("Reservierung darf nicht null sein");
 		}
 		boatReservations.add(boatReservation);
 	}
-	
-	public void setBoatReservations(List<BoatReservation> boatReservations) throws RowBuddyException {
-		if (boatReservations == null){
-			throw new NullPointerException("BoatReservations cannot be null");
+
+	public void setBoatReservations(List<BoatReservation> boatReservations)
+			throws RowBuddyException {
+		if (boatReservations == null) {
+			throw new NullPointerException("Reservierung darf nicht null sein");
 		}
 		this.boatReservations = boatReservations;
 	}
-	
+
 	public boolean isLocked() {
 		return this.locked;
 	}
 
 	public void setLocked(boolean locked) {
 		this.locked = locked;
-	}   
-	
+	}
+
 	public boolean isDeleted() {
 		return this.deleted;
 	}
@@ -140,16 +148,17 @@ public class Boat implements Serializable {
 	public void setDeleted(boolean deleted) {
 		this.deleted = deleted;
 	}
-	
-	public void validate() throws RowBuddyException{
-		if (name.isEmpty()){
-			throw new RowBuddyException("Name has to be set");
+
+	public void validate() throws RowBuddyException {
+		if (name.isEmpty()) {
+			throw new RowBuddyException("Der Name muss gesetzt sein");
 		}
-		if(numberOfSeats <= 0) {
-			throw new RowBuddyException("There has to be at least 1 seat");
+		if (numberOfSeats <= 0) {
+			throw new RowBuddyException(
+					"Das Boot muss mindestens einen Sitzplatz haben");
 		}
 	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -157,6 +166,7 @@ public class Boat implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
