154b7f23e35ba4357a03e85576b1965b763d4948
fleischer.georg@gmail.com <fleischer.georg@gmail.com@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-21 20:30:09 +0000

    ServerCallHandler in LogRowedTripPresenter & MemberSuggestOracle

--- trunk/20_sources/Web Project/src/de/rowbuddy/client/logbook/MemberSuggestOracle.java
+++ trunk/20_sources/Web Project/src/de/rowbuddy/client/logbook/MemberSuggestOracle.java
@@ -4,10 +4,10 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.SuggestOracle;
 
 import de.rowbuddy.boundary.dtos.MemberDTO;
+import de.rowbuddy.client.ServerRequestHandler;
 import de.rowbuddy.client.services.LogbookRemoteServiceAsync;
 
 public class MemberSuggestOracle extends SuggestOracle {
@@ -43,7 +43,7 @@ public class MemberSuggestOracle extends SuggestOracle {
 		}
 
 	}
-	
+
 	public int getNumberOfSuggestions() {
 		return this.suggestions.size();
 	}
@@ -56,24 +56,22 @@ public class MemberSuggestOracle extends SuggestOracle {
 	public void requestSuggestions(Request req, Callback arg1) {
 		this.request = req;
 		this.callback = arg1;
-		service.searchMember(req.getQuery(), new AsyncCallback<List<MemberDTO>>() {
+		service.searchMember(req.getQuery(),
+				new ServerRequestHandler<List<MemberDTO>>(null,
+						"Mitglieder suchen", null, null) {
 
-			@Override
-			public void onSuccess(List<MemberDTO> arg0) {
-				suggestions.clear();
-				SuggestOracle.Response resp = new SuggestOracle.Response();
-				List<Suggestion> suggs = new LinkedList<SuggestOracle.Suggestion>();
-				for (MemberDTO member : arg0) {
-					suggestions.put(member.getFullName(), member);
-					suggs.add(new ItemSuggestion(member.getFullName()));
-				}
-				resp.setSuggestions(suggs);
-				callback.onSuggestionsReady(request, resp);
-			}
-
-			@Override
-			public void onFailure(Throwable arg0) {
-			}
-		});
+					@Override
+					public void onSuccess(List<MemberDTO> arg0) {
+						suggestions.clear();
+						SuggestOracle.Response resp = new SuggestOracle.Response();
+						List<Suggestion> suggs = new LinkedList<SuggestOracle.Suggestion>();
+						for (MemberDTO member : arg0) {
+							suggestions.put(member.getFullName(), member);
+							suggs.add(new ItemSuggestion(member.getFullName()));
+						}
+						resp.setSuggestions(suggs);
+						callback.onSuggestionsReady(request, resp);
+					}
+				});
 	}
 }
