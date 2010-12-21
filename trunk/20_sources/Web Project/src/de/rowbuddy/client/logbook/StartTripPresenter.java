79b1eea67ff2b7239b97c42abc996c843cead959
fleischer.georg@gmail.com <fleischer.georg@gmail.com@834b1e41-3d65-c98c-e898-4ec1c73a669e>
2010-12-21 20:36:29 +0000

    ServerCallHandler in StartTripPresenter

--- trunk/20_sources/Web Project/src/de/rowbuddy/client/logbook/StartTripPresenter.java
+++ trunk/20_sources/Web Project/src/de/rowbuddy/client/logbook/StartTripPresenter.java
@@ -11,7 +11,6 @@ import com.google.gwt.event.dom.client.HasClickHandlers;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.EventBus;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.HasValue;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.ListBox;
@@ -25,13 +24,13 @@ import de.rowbuddy.boundary.dtos.RouteDTO;
 import de.rowbuddy.boundary.dtos.TripDTO;
 import de.rowbuddy.boundary.dtos.TripMemberDTO;
 import de.rowbuddy.client.Presenter;
+import de.rowbuddy.client.ServerRequestHandler;
 import de.rowbuddy.client.events.ListPersonalOpenTripsEvent;
 import de.rowbuddy.client.events.ListPersonalTripsEvent;
 import de.rowbuddy.client.events.StatusMessageEvent;
 import de.rowbuddy.client.model.StatusMessage;
 import de.rowbuddy.client.model.StatusMessage.Status;
 import de.rowbuddy.client.services.LogbookRemoteServiceAsync;
-import de.rowbuddy.entities.Trip;
 import de.rowbuddy.entities.TripMemberType;
 
 public class StartTripPresenter implements Presenter {
@@ -44,7 +43,7 @@ public class StartTripPresenter implements Presenter {
 		HasClickHandlers getCancelButton();
 
 		void setRouteOracle(SuggestOracle oracle);
-		
+
 		void setMemberOracle(SuggestOracle oracle);
 
 		void setBoatOracle(SuggestOracle oracle);
@@ -54,15 +53,15 @@ public class StartTripPresenter implements Presenter {
 		HasValue<String> getBoatName();
 
 		HasValue<String> getMemberName();
-		
+
 		SuggestBox getMember();
-		
+
 		public void showTripMembers(String[] tripMembers);
 
 		ListBox getListBox();
 
 		void setBoatInformation(String name, boolean coxed, int numberOfSeats);
-		
+
 		void setRouteInformation(double lenght);
 
 		HasClickHandlers getDeleteTripMemberButton();
@@ -71,7 +70,8 @@ public class StartTripPresenter implements Presenter {
 
 	}
 
-	private static Logger logger = Logger.getLogger(StartTripPresenter.class.getName());
+	private static Logger logger = Logger.getLogger(StartTripPresenter.class
+			.getName());
 	private Display view;
 	private LogbookRemoteServiceAsync logbookService;
 	private EventBus eventBus;
@@ -85,7 +85,7 @@ public class StartTripPresenter implements Presenter {
 		this.view = view;
 		this.logbookService = service;
 		this.eventBus = eventBus;
-		this.tripMembers = new LinkedList<TripMemberDTO>(); 
+		this.tripMembers = new LinkedList<TripMemberDTO>();
 		routeOracle = new RouteSuggestOracle(service);
 		boatOracle = new BoatSuggestOracle(service);
 		memberOracle = new MemberSuggestOracle(service);
@@ -102,92 +102,85 @@ public class StartTripPresenter implements Presenter {
 		view.setRouteOracle(routeOracle);
 		view.setMemberOracle(memberOracle);
 		view.setBoatOracle(boatOracle);
-		
-		view.getMember().addValueChangeHandler(new ValueChangeHandler<String>() {
-			
-			@Override
-			public void onValueChange(ValueChangeEvent<String> arg0) {
-				MemberDTO member = memberOracle.getSuggestion(view.getMemberName().getValue());
-				
-				if(member != null) {
-					view.getMemberName().setValue("");
-					TripMemberDTO tm = new TripMemberDTO();
-					tm.setMember(member);
-					for (TripMemberDTO dto : tripMembers) {
-						if(dto.getMember().getId() == member.getId()) {
-							return;
+
+		view.getMember().addValueChangeHandler(
+				new ValueChangeHandler<String>() {
+
+					@Override
+					public void onValueChange(ValueChangeEvent<String> arg0) {
+						MemberDTO member = memberOracle.getSuggestion(view
+								.getMemberName().getValue());
+
+						if (member != null) {
+							view.getMemberName().setValue("");
+							TripMemberDTO tm = new TripMemberDTO();
+							tm.setMember(member);
+							for (TripMemberDTO dto : tripMembers) {
+								if (dto.getMember().getId() == member.getId()) {
+									return;
+								}
+							}
+							BoatDTO boat = boatOracle.getSuggestion(view
+									.getBoatName().getValue());
+							if (boat == null) {
+								StatusMessage message = new StatusMessage(false);
+								message.setStatus(Status.NEGATIVE);
+								message.setMessage("Es muss ein Boot ausgewählt sein");
+								eventBus.fireEvent(new StatusMessageEvent(
+										message));
+							} else {
+								int maxPlacesInBoat = boat.getNumberOfSeats();
+								if (boat.isCoxed()) {
+									++maxPlacesInBoat;
+								}
+								if (tripMembers.size() >= maxPlacesInBoat) {
+									return;
+								}
+							}
+							tm.setTripMemberType(TripMemberType.Rower);
+							tripMembers.add(tm);
+
+							updateTripMemberList();
 						}
 					}
-					BoatDTO boat = boatOracle.getSuggestion(view.getBoatName().getValue());
-					if (boat == null) {
-						StatusMessage message = new StatusMessage(false);
-						message.setStatus(Status.NEGATIVE);
-						message.setMessage("Es muss ein Boot ausgewählt sein");
-						eventBus.fireEvent(new StatusMessageEvent(message));
-					} else {
-						int maxPlacesInBoat = boat.getNumberOfSeats();
-						if(boat.isCoxed()) {
-							++maxPlacesInBoat;
-						}
-						if (tripMembers.size() >= maxPlacesInBoat ) {
-							return;
+				});
+
+		view.getBoatName().addValueChangeHandler(
+				new ValueChangeHandler<String>() {
+					@Override
+					public void onValueChange(ValueChangeEvent<String> arg0) {
+						BoatDTO boat = boatOracle.getSuggestion(view
+								.getBoatName().getValue());
+						if (boat != null) {
+							for (TripMemberDTO dto : tripMembers) {
+								dto.setTripMemberType(TripMemberType.Rower);
+							}
+							view.setBoatInformation(boat.getName(),
+									boat.isCoxed(), boat.getNumberOfSeats());
 						}
 					}
-					tm.setTripMemberType(TripMemberType.Rower);
-					tripMembers.add(tm);
-					
-					updateTripMemberList();		
-				}
-			}
-		});
-		
-		view.getBoatName().addValueChangeHandler(new ValueChangeHandler<String>() {
-			@Override
-			public void onValueChange(ValueChangeEvent<String> arg0) {
-				BoatDTO boat = boatOracle.getSuggestion(view.getBoatName().getValue());
-				if(boat != null) {
-					for (TripMemberDTO dto : tripMembers) {
-						dto.setTripMemberType(TripMemberType.Rower);
+
+				});
+
+		view.getRouteName().addValueChangeHandler(
+				new ValueChangeHandler<String>() {
+					@Override
+					public void onValueChange(ValueChangeEvent<String> arg0) {
+						RouteDTO route = routeOracle.getSuggestion(view
+								.getRouteName().getValue());
+						if (route != null) {
+							view.setRouteInformation(route.getLengthKM());
+						}
 					}
-					view.setBoatInformation(boat.getName(), boat.isCoxed(), boat.getNumberOfSeats());
-				}
-			}
-			
-		});
-		
-		view.getRouteName().addValueChangeHandler(new ValueChangeHandler<String>() {
-			@Override
-			public void onValueChange(ValueChangeEvent<String> arg0) {
-				RouteDTO route = routeOracle.getSuggestion(view.getRouteName().getValue());
-				if(route != null) {
-					view.setRouteInformation(route.getLengthKM());
-				}
-			}
-			
-		});
+
+				});
 
 		view.getAddButton().addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent arg0) {
-				startTrip(new AsyncCallback<Trip>() {
-
-					@Override
-					public void onFailure(Throwable arg0) {
-						logger.severe("Cannot log trip: " + arg0.getMessage());
-					}
-
-					@Override
-					public void onSuccess(Trip arg0) {
-						logger.info("Trip successful logged; Reset View");
-						eventBus.fireEvent(new ListPersonalOpenTripsEvent());
-						StatusMessage message = new StatusMessage();
-						message.setMessage("Fahrt erfolgreich gestartet");
-						message.setStatus(Status.POSITIVE);
-						message.setAttached(false);
-						eventBus.fireEvent(new StatusMessageEvent(message));
-					}
-				});
+				startTrip(new ServerRequestHandler<Void>(eventBus,
+						"Fahrt starten", new ListPersonalOpenTripsEvent(), null));
 			}
 		});
 
@@ -198,8 +191,8 @@ public class StartTripPresenter implements Presenter {
 				eventBus.fireEvent(new ListPersonalTripsEvent());
 			}
 		});
-		
-		view.getDeleteTripMemberButton().addClickHandler(new ClickHandler() {			
+
+		view.getDeleteTripMemberButton().addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent arg0) {
 				int index = view.getListBox().getSelectedIndex();
@@ -209,11 +202,12 @@ public class StartTripPresenter implements Presenter {
 				updateTripMemberList();
 			}
 		});
-		
-		view.getSetCoxButton().addClickHandler(new ClickHandler() {			
+
+		view.getSetCoxButton().addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent arg0) {
-				BoatDTO boat = boatOracle.getSuggestion(view.getBoatName().getValue());
+				BoatDTO boat = boatOracle.getSuggestion(view.getBoatName()
+						.getValue());
 				if (boat == null || !boat.isCoxed()) {
 					StatusMessage message = new StatusMessage(false);
 					message.setStatus(Status.NEGATIVE);
@@ -225,7 +219,8 @@ public class StartTripPresenter implements Presenter {
 						for (TripMemberDTO dto : tripMembers) {
 							dto.setTripMemberType(TripMemberType.Rower);
 						}
-						tripMembers.get(index).setTripMemberType(TripMemberType.Cox);
+						tripMembers.get(index).setTripMemberType(
+								TripMemberType.Cox);
 					}
 					updateTripMemberList();
 				}
@@ -233,19 +228,21 @@ public class StartTripPresenter implements Presenter {
 		});
 
 	}
-	
+
 	private void updateTripMemberList() {
 		String[] tms = new String[tripMembers.size()];
-		for (int i = 0; i< tripMembers.size(); i++) {
+		for (int i = 0; i < tripMembers.size(); i++) {
 			tms[i] = tripMembers.get(i).getMember().getFullName();
-			if(tripMembers.get(i).getTripMemberType().equals(TripMemberType.Cox)) {
+			if (tripMembers.get(i).getTripMemberType()
+					.equals(TripMemberType.Cox)) {
 				tms[i] += " (Cox)";
 			}
 		}
 		view.showTripMembers(tms);
 	}
-	private void startTrip(AsyncCallback<Trip> action) {
-		
+
+	private void startTrip(ServerRequestHandler<Void> action) {
+
 		BoatDTO boat = boatOracle.getSuggestion(view.getBoatName().getValue());
 		if (boat == null) {
 			StatusMessage message = new StatusMessage(false);
@@ -253,7 +250,8 @@ public class StartTripPresenter implements Presenter {
 			message.setMessage("Es muss ein Boot ausgewählt sein");
 			eventBus.fireEvent(new StatusMessageEvent(message));
 		} else {
-			RouteDTO route = routeOracle.getSuggestion(view.getRouteName().getValue());
+			RouteDTO route = routeOracle.getSuggestion(view.getRouteName()
+					.getValue());
 			if (route == null) {
 				StatusMessage message = new StatusMessage(false);
 				message.setStatus(Status.NEGATIVE);
@@ -270,30 +268,8 @@ public class StartTripPresenter implements Presenter {
 					try {
 						trip.setFinished(true);
 						trip.setStartDate(new Date(System.currentTimeMillis()));
-						logbookService.startTrip(trip, boat.getId(), route.getId(), this.tripMembers , 
-								new AsyncCallback<Void>() {
-
-							@Override
-							public void onFailure(Throwable arg0) {
-								StatusMessage message = new StatusMessage(
-										false);
-								message.setStatus(Status.NEGATIVE);
-								message.setMessage(arg0.getMessage());
-								eventBus.fireEvent(new StatusMessageEvent(
-										message));
-							}
-
-							@Override
-							public void onSuccess(Void arg0) {
-								StatusMessage message = new StatusMessage(
-										false);
-								message.setStatus(Status.POSITIVE);
-								message.setMessage("Trip erfolgreich hinzugefügt");
-								eventBus.fireEvent(new ListPersonalTripsEvent());
-								eventBus.fireEvent(new StatusMessageEvent(
-										message));
-							}
-						});
+						logbookService.startTrip(trip, boat.getId(),
+								route.getId(), this.tripMembers, action);
 					} catch (Exception e) {
 						StatusMessage message = new StatusMessage();
 						message.setMessage(e.getMessage());
@@ -302,8 +278,8 @@ public class StartTripPresenter implements Presenter {
 						eventBus.fireEvent(new StatusMessageEvent(message));
 						logger.warning(e.getMessage());
 					}
-				}				
-			}			
+				}
+			}
 		}
 	}
 }
